280506f5c4c5e4a9df91bbf60eb4beb4febc4d2e
TheHalalPigeon <125734418+TheHalalPigeon@users.noreply.github.com>
2024-03-10 16:41:19 +0000

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 const ShortUrl = require("./models/shortUrl");
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 
 const connectDB = async () => {
   try {
