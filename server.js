69d481d20f9771153ba0f0ecd87ec8117fe5f0c2
TheHalalPigeon <125734418+TheHalalPigeon@users.noreply.github.com>
2024-03-10 18:38:24 +0000

    Update server.js

--- server.js
+++ server.js
@@ -1,47 +1,34 @@
-require("dotenv").config();
-const express = require("express");
-const mongoose = require("mongoose");
-const ShortUrl = require("./models/shortUrl");
+const express = require('express')
+const mongoose = require('mongoose')
+const ShortUrl = require('./models/shortUrl')
+const app = express()
 
-const app = express();
-const PORT = process.env.PORT || 3000;
+mongoose.connect(process.env.DATABASE_URL, {
+  useNewUrlParser: true, useUnifiedTopology: true
+})
 
-const connectDB = async () => {
-  try {
-    await mongoose.connect(process.env.DATABASE_URL);
-    console.log("MongoDB Connected");
-    return;
-  } catch (error) {
-    console.log(error);
-    process.exit(1);
-  }
-}
+app.set('view engine', 'ejs')
+app.use(express.urlencoded({ extended: false }))
 
-app.set("view engine", "ejs");
-app.use(express.urlencoded({ extended: false }));
+app.get('/', async (req, res) => {
+  const shortUrls = await ShortUrl.find()
+  res.render('index', { shortUrls: shortUrls })
+})
 
-app.get("/", async (req, res) => {
-  const shortUrls = await ShortUrl.find({});
-  res.render("index", { shortUrls: shortUrls });
-});
+app.post('/shortUrls', async (req, res) => {
+  await ShortUrl.create({ full: req.body.fullUrl })
 
-app.post("/shortUrls", async (req, res) => {
-  await ShortUrl.create({ full: req.body.fullUrl });
-  res.redirect("/");
-});
+  res.redirect('/')
+})
 
-app.get("/:shortUrl", async (req, res) => {
-  const shortUrl = await ShortUrl.findOne({ short: req.params.shortUrl });
-  if (shortUrl == null) return res.sendStatus(404);
+app.get('/:shortUrl', async (req, res) => {
+  const shortUrl = await ShortUrl.findOne({ short: req.params.shortUrl })
+  if (shortUrl == null) return res.sendStatus(404)
 
-  shortUrl.clicks++;
-  shortUrl.save();
+  shortUrl.clicks++
+  shortUrl.save()
 
-  res.redirect(shortUrl.full);
-});
+  res.redirect(shortUrl.full)
+})
 
-connectDB().then(() => {
-  app.listen(PORT, () => {
-    console.log("listening for requests");
-  });
-});
+app.listen(process.env.PORT || 5000);
